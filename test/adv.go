82023fc135d4c90d40237536aeed0c70e3ef17fa
datafridge <feschmi.jena@gmail.com>
2016-06-14 10:57:00 +0200

    add test

---
+++ test/adv.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	//"encoding/json"
+	//"github.com/godbus/dbus"
+	//"github.com/godbus/dbus/introspect"
+	//"os"
+	//"fmt"
+)
+
+type advertisement_package struct {
+    ad_type string
+    ad_serviceUUIDs []string
+    manufacturerData map[uint8]uint8
+    solicitUUIDs []string
+    serviceData map[uint8]uint8
+    includeTxPower bool
+}
+
+func main() {
+
+}
