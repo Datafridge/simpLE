6781143346d4519cbd3414d00ca70265bc804d1e
Datafridge <felix.schmidt@uni-weimar.de>
2016-07-03 16:43:56 +0200

    screenshot

--- advertisement.go
+++ advertisement.go
@@ -8,7 +8,13 @@ import (
 	//"fmt"
 )
 
-// structure of advertising package
+var BLUEZ_SERVICE_NAME = "org.bluez"
+var LE_ADVERTISING_MANAGER_IFACE = "org.bluez.LEAdvertisingManager1"
+var DBUS_OM_IFACE = "org.freedesktop.DBus.ObjectManager"
+var DBUS_PROP_IFACE = "org.freedesktop.DBus.Properties"
+
+var LE_ADVERTISEMENT_IFACE = "org.bluez.LEAdvertisement1"
+
 type advertisement_package struct {
     ad_type string
     ad_serviceUUIDs []string
