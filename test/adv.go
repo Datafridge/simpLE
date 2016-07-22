6a4f35debc9cbbe3194fc640d55a75e0a40d5e9a
Datafridge <felix.schmidt@uni-weimar.de>
2016-07-22 11:54:59 +0200

    clean up adv test

--- test/adv.go
+++ test/adv.go
@@ -6,6 +6,7 @@ import (
 	"github.com/godbus/dbus/prop"
 	"github.com/godbus/dbus/introspect"
 	//"os"
+	"time"
 	"fmt"
 )
 
@@ -58,49 +59,26 @@ func main() {
 		panic(err)
 	}
 
-	/*sbus, err := dbus.SessionBus()
-	if err != nil {
-		panic(err)
-	}*/
-
 	//get adapter
 	adapter_path := dbus.ObjectPath("/org/bluez/hci0")
 	adapter := bus.Object(BLUEZ_SERVICE_NAME,adapter_path)
-	//fmt.Println(adapter.Path())
-	//fmt.Println(adapter.Destination())
-	//address,err := adapter.GetProperty("org.bluez.Adapter1.Alias")
-	//fmt.Printf("value: %v, err: %v \n",address.String(),err)
-	//fmt.Printf("value: %T, err: %v \n \n",address,err)
 
 	var result interface{}
 	powered1 := true
 	err = adapter.Call("org.freedesktop.DBus.Properties.Set", 0, "org.bluez.Adapter1", "Powered", dbus.MakeVariant(powered1)).Store(&result)
-	//fmt.Printf("result value: %v, type: %T \n",result,result)
-	//fmt.Printf("error  value: %v, type: %T \n",err,err)
-
-	//adapter_manager_path := dbus.ObjectPath("/org/bluez/hci0")
-	//adapter := bus.Object(BLUEZ_SERVICE_NAME,adapter_manager_path)
 
 	adv := new(advertisement_package)
 
-	adv.ad_type = "broadcast"
+	adv.ad_type = "peripheral"
 	adv.ad_serviceUUIDs = []string{"180D","180F"}
 	adv.ad_manufacturerData = map[uint16][]uint8{0xFFFF:{0x00, 0x01, 0x02, 0x03, 0x04}}
 	//adv.ad_solicitUUIDs = make([]string,0)
 	adv.ad_serviceData = map[string][]uint8{"9999":{0x00, 0x01, 0x02, 0x03, 0x04}}
 	adv.ad_includeTxPower = true
 
-	/*adv.ad_type = "peripheral"
-	adv.ad_serviceUUIDs = []string{"180D","180F"}
-	adv.ad_manufacturerData = map[uint16][]uint8{0xFFFF:{0x00, 0x01, 0x02, 0x03, 0x04}}
-	//adv.ad_solicitUUIDs = make([]string,0)
-	adv.ad_serviceData = map[string][]uint8{"9999":{0x00, 0x01, 0x02, 0x03, 0x04}}
-	adv.ad_includeTxPower = true*/
-
 	//TODO: request name before add new object
 
 	advertisement_path := dbus.ObjectPath("/org/bluez/simpLE/advertisement1")
-	//advertisement1 := bus.Object(BLUEZ_SERVICE_NAME,advertisement_path)
 	f := foo("Bar")
 	bus.Export(f, advertisement_path, "org.bluez.LEAdvertisement1")
 
@@ -144,7 +122,7 @@ func main() {
 					return nil
 				},
 			},
-			/*"ServiceData": {
+			"ServiceData": {
 				map[string][]uint8(adv.ad_serviceData),
 				true,
 				prop.EmitTrue,
@@ -152,7 +130,7 @@ func main() {
 					fmt.Println(c.Name, "changed to", c.Value)
 					return nil
 				},
-			},*/
+			},
 			"IncludeTxPower": {
 				bool(adv.ad_includeTxPower),
 				true,
@@ -180,20 +158,9 @@ func main() {
 	}
 	bus.Export(introspect.NewIntrospectable(n), "/org/bluez/simpLE/advertisement1","org.freedesktop.DBus.Introspectable")
 
-	//fmt.Println("org.bluez.LEAdvertisement1 / /org/bluez/simpLE/advertisement1 ...")
-
-	/*c := make(chan *dbus.Signal)
-	bus.Signal(c)
-	for _ = range c {
-	}*/
-
 	var dic map[string]dbus.Variant
 	err = adapter.Call("org.bluez.LEAdvertisingManager1.RegisterAdvertisement", 0, advertisement_path, dic).Store(&result)
+	time.Sleep(2 * time.Minute)
 	fmt.Printf("result: value: %T, err: %v \n \n",result,result)
 	fmt.Printf("err: value: %T, err: %v \n \n",err,err)
-
-	//value1,err := advertisement1.GetProperty("org.bluez.LEAdvertisement1.Type")
-	//fmt.Printf("value1: value: %T, err: %v \n \n",value1.String(),value1.String())
-
-
 }
