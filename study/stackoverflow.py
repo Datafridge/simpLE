a5f949066831482699d8ae79030251902268111a
Datafridge <felix.schmidt@uni-weimar.de>
2016-07-31 11:46:09 +0200

    stackoverflow analysis

--- study/stackoverflow.py
+++ study/stackoverflow.py
@@ -2,25 +2,33 @@
 
 from datetime import datetime, date, timedelta
 import requests
+import json
 
 def main(tag):
-    actual = datetime(2016,7,25)
+    actual = datetime(2010,1,1)
     end    = datetime(2016,7,30)
     total  = end - actual
-    f = open(tag+"_"+str(actual)+"_"+str(end), 'w')
+    filename = tag+"_"+str(actual)+"_"+str(end)
+    f = open(filename, 'w')
+    f.close()
+
 
     while True:
+        f = open(filename, 'a')
         d = end - actual
-        print("progress:"+str(total.days-d.days)+"/"+str(total.days), end="\r")
         f.write(get_data(tag,actual))
+        f.close()
+        print("progress:"+str(total.days-d.days)+"/"+str(total.days)+" "+str(quota_remaining)+" requests left", end="\r")
         if actual == end:
             break
         actual = actual+timedelta(1)
 
 def get_data(tag,actual):
-    return str(create_request(int(actual.timestamp()),int((actual+timedelta(1)).timestamp()),tag))
-    #TODO Format JSON
-
+    entry = create_request(int(actual.timestamp()),int((actual+timedelta(1)).timestamp()),tag)
+    global quota_remaining
+    quota_remaining = entry["quota_remaining"]
+    #print  (str(actual.year)+"-"+str(actual.month)+"-"+str(actual.day)+": "+str(entry["total"])+"\n")
+    return (str(actual.year)+"-"+str(actual.month)+"-"+str(actual.day)+": "+str(entry["total"])+"\n")
 
 def create_request(from_date,to_date,tag):
     r = requests.get("https://api.stackexchange.com/2.2/search?fromdate="+str(from_date)+"&todate="+str(to_date)+"&order=desc&sort=creation&tagged="+tag+"&site=stackoverflow&filter=!.UE8F0_CXHtXgLFC&key=maUPfGS71Y2I2AkkdnMpVw((")
@@ -28,4 +36,4 @@ def create_request(from_date,to_date,tag):
     return(r.json())
 
 if __name__ == "__main__":
-    main("bluetooth-lowenergy")
+    main("bluetooth")
