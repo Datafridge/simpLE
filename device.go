171fd114d4d73fd9acb19b97d6500a28aad86f42
Datafridge <felix.schmidt@uni-weimar.de>
2016-09-01 17:09:01 +0200

    add scanner

--- device.go
+++ device.go
@@ -1,8 +1,20 @@
 package simpLE
 
-import ()
+import (
+    "github.com/godbus/dbus"
+)
 
 type device struct {
     path string
-    name string
+}
+
+func (d *device) dbusobject()  dbus.BusObject{
+    bus, err := dbus.SystemBus()
+	if err != nil {
+		panic(err)
+	}
+
+    adapter_path := dbus.ObjectPath("/org/bluez/hci0")
+	adapter := bus.Object("org.bluez",adapter_path)
+    return adapter
 }
---
+++ scanner.go
@@ -0,0 +1,19 @@
+package simpLE
+
+import (
+    //"errors"
+    //"fmt"
+)
+
+type scanner struct {
+    dev *device
+    f   func()//TODO: Add structure for received advertisements
+}
+
+func (s *scanner) device(string error) {
+
+}
+
+func (s *scanner) Start() error {
+    return nil
+}
