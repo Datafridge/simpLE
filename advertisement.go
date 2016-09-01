131e2214a3c09128f7398bafe653a08354f07dea
Datafridge <felix.schmidt@uni-weimar.de>
2016-09-01 18:00:05 +0200

    add simpLE

--- advertisement.go
+++ advertisement.go
@@ -1,6 +1,11 @@
 package simpLE
 
-import ()
+import (
+    "github.com/godbus/dbus"
+    "github.com/godbus/dbus/prop"
+    "github.com/godbus/dbus/introspect"
+    "fmt"
+)
 
 type advertisement struct {
     ad_path string
@@ -27,4 +32,83 @@ func (adv *advertisement) add_manufacturerData(manuf_code uint16, data []uint8) 
 
 func (adv *advertisement) add_serviceData(uuid string, data []uint8) {
     adv.ad_serviceData[uuid] = data
+}
+
+func (adv *advertisement) register()  {
+    propsSpec := map[string]map[string]*prop.Prop{
+        "org.bluez.LEAdvertisement1": {
+            "Type": {
+                string(adv.ad_type),
+                true,
+                prop.EmitTrue,
+                func(c *prop.Change) *dbus.Error {
+                    fmt.Println(c.Name, "changed to", c.Value)
+                    return nil
+                },
+            },
+            "ServiceUUIDs": {
+                []string(adv.ad_serviceUUIDs),
+                true,
+                prop.EmitTrue,
+                func(c *prop.Change) *dbus.Error {
+                    fmt.Println(c.Name, "changed to", c.Value)
+                    return nil
+                },
+            },
+            "ManufacturerData": {
+                map[uint16][]uint8(adv.ad_manufacturerData),
+                true,
+                prop.EmitTrue,
+                func(c *prop.Change) *dbus.Error {
+                    fmt.Println(c.Name, "changed to", c.Value)
+                    return nil
+                },
+            },
+            "SolicitUUIDs": {
+                []string(adv.ad_solicitUUIDs),
+                true,
+                prop.EmitTrue,
+                func(c *prop.Change) *dbus.Error {
+                    fmt.Println(c.Name, "changed to", c.Value)
+                    return nil
+                },
+            },
+            "ServiceData": {
+                map[string][]uint8(adv.ad_serviceData),
+                true,
+                prop.EmitTrue,
+                func(c *prop.Change) *dbus.Error {
+                    fmt.Println(c.Name, "changed to", c.Value)
+                    return nil
+                },
+            },
+            "IncludeTxPower": {
+                bool(adv.ad_includeTxPower),
+                true,
+                prop.EmitTrue,
+                func(c *prop.Change) *dbus.Error {
+                    fmt.Println(c.Name, "changed to", c.Value)
+                    return nil
+                },
+            },
+        },
+    }
+
+    props := prop.New(bus, "/org/bluez/simpLE/advertisement1", propsSpec)
+
+    n := &introspect.Node{
+		Name: "/org/bluez/simpLE/advertisement1",
+		Interfaces: []introspect.Interface{
+			introspect.IntrospectData,
+			prop.IntrospectData,
+			{
+				Name:       "org.bluez.LEAdvertisement1",
+				Methods:    introspect.Methods(f),
+				Properties: props.Introspection("org.bluez.LEAdvertisement1"),
+			},
+		},
+	}
+
+    
+
 }
---
+++ simpLE.go
@@ -0,0 +1,15 @@
+package simpLE
+
+import ()
+
+type simpLE struct {
+    id int
+}
+
+func (s *simpLE) Start() {
+
+}
+
+func convert2hex(a int) {
+
+}
